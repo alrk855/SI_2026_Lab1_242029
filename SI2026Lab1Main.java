127f7d6ac6b76d9fdc46cf9e4d1a7a0f7113e69a
Alrk855 <110738882+alrk855@users.noreply.github.com>
2026-03-28 20:18:33 +0100

    Implement book search

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -52,9 +52,13 @@ class Library {
     }
 
     // TODO: Implement in branch feature-search-books
-    public boolean searchBookByTitle(String title) {
-        return false;
+ public boolean searchBookByTitle(String title) {
+    for (Book book : books) {
+        if (book.getTitle().equalsIgnoreCase(title)) {
+            return true;
+        }
     }
+    return false;
 
     // TODO: Implement in branch feature-borrow-book
     public void borrowBook(String title) {
@@ -120,5 +124,6 @@ public class SI2026Lab1Main {
         library.addBook(new Book("1984", "George Orwell", "Dystopian"));
 
         System.out.println("Library initialized.");
+        System.out.println(library.searchBookByTitle("1984"));
     }
 }
